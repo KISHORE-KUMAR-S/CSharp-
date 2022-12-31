59709be81fe27daa59d57ac9cb80e2e2f344a2a5
KISHORE KUMAR S <77736919+KISHORE-KUMAR-S@users.noreply.github.com>
2022-12-31 18:36:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your DOB: ");
+DateTime MyAge = Convert.ToDateTime(Console.ReadLine());
+int years = Convert.ToInt32(DateTime.Now.Subtract(MyAge).TotalDays)/360;
+Console.WriteLine("Your age is: " + years);                                             
